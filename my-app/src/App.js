8e6c61ea4b1a7438ab96f0660a58005a3c71c4ea
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2022-12-11 20:45:55 +0100

    Repo setup & concepts for files structure

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Searchbar from './components/Searchbar/Searchbar';
+
+
+function App() {
+  return (
+    <div>
+      <Searchbar></Searchbar>
+     
+      </div>
+      
+
+  );
+}
+
+export default App;
